353a71217f20be4646883350eaca06b69ec2457f
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-11-20 23:06:09 -0800

    enhance keyboard controls

--- components/Ball.tsx
+++ components/Ball.tsx
@@ -1,40 +1,58 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Ball = () => {
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+  const arrowLeftDown = useRef<boolean>(false);
+  const arrowRightDown = useRef<boolean>(false);
+
+  const setImmediate = (func: () => void, interval: number): NodeJS.Timer => {
+    func();
+    return setInterval(func, interval);
+  };
+
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
       if (e.key === "ArrowLeft") {
-        moveLeft();
+        if (e.repeat || arrowRightDown.current === true) return;
+        arrowLeftDown.current = true;
+        intervalRef.current = setImmediate(() => moveLeft(), 1);
       }
       if (e.key === "ArrowRight") {
-        moveRight();
+        if (e.repeat || arrowLeftDown.current === true) return;
+        arrowRightDown.current = true;
+        intervalRef.current = setImmediate(() => moveRight(), 1);
+      }
+    });
+
+    document.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowLeft" && arrowLeftDown.current === true) {
+        arrowLeftDown.current = false;
+        intervalRef.current && clearInterval(intervalRef.current);
+      }
+      if (e.key === "ArrowRight" && arrowRightDown.current === true) {
+        arrowRightDown.current = false;
+        intervalRef.current && clearInterval(intervalRef.current);
       }
     });
   }, []);
 
-  const ballRef = useRef(null);
+  const ballRef = useRef<null | HTMLDivElement>(null);
 
   const moveLeft = () => {
-    console.log(getComputedStyle(ballRef.current));
     if (ballRef.current !== null) {
       const currentLeftPosition = parseInt(
-        getComputedStyle(ballRef.current).getPropertyValue("left")
+        getComputedStyle(ballRef.current).getPropertyValue("right")
       );
-      ballRef.current.style.left = currentLeftPosition - 2 + "px";
+      ballRef.current.style.right = currentLeftPosition + 2 + "px";
     }
   };
 
   const moveRight = () => {
     if (ballRef.current !== null) {
-      const currentLeftPosition = parseInt(
-        getComputedStyle(ballRef.current).getPropertyValue("left")
+      const currentRightPosition = parseInt(
+        getComputedStyle(ballRef.current).getPropertyValue("right")
       );
-      const currentRightPosition =
-        ballRef.current.getBoundingClientRect().right;
       ballRef.current.style.right = currentRightPosition - 2 + "px";
-      ballRef.current.style.left = currentRightPosition + 2 + "px";
-      console.log(ballRef.current.style.right);
-      console.log(ballRef.current.style.left);
     }
   };
 
